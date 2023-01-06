59915b3fbffa0e61e07105202d32d9c7296a8169
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-01-06 21:48:35 +0900

    Time: 1681 ms (52.40%), Space: 14 MB (31.26%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,17 +6,20 @@ class Solution:
                 r+=1 
             return r-(l+1), l+1
         
+        # 예외 처리1 : string이 1글자짜리나 0글자짜리
         if len(s) <= 1:
             return s
+        #예외 처리2 : string이 2글자짜리 
         elif len(s) == 2:
             if s[0] == s[1]:
                 return s
             else:
                 return s[0]
+        # 일반적인 케이스 
         else:
             length = 0
             for i in range(len(s)):
-                len1,l1 = check(s,i,i)
+                len1,l1 = check(s,i,i) #l = r일 수 있는 게 포인트 
                 len2,l2 = check(s,i,i+1)
                 
                 len_i = max(len1,len2)
