7d8787e194d099a3e5edb90024448e9de751b380
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-01-13 18:25:31 +0900

    Time: 32 ms (83.99%), Space: 13.9 MB (68.93%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,55 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) ==1:
+            return False 
+        
+        
+        
+        brackets = {"(":")","{":"}","[":"]"}
+        reverse_brackets = {")":"(", "}":"{", "]":"["}
+        left = set( ["(", "{", "["] )
+        valid = []
+            
+
+            
+        
+        for char in s[::-1]:
+            if char in brackets.values():
+                valid.append(char)
+            elif valid and brackets[char] == valid[-1]:
+                valid.pop()
+            else:
+                return False
+        
+        return valid == []
+            
+#             # print(reverse_brackets[char])
+            
+#             if (char in reverse_brackets):
+#                 if (reverse_brackets[char] in valid.keys()):
+#                     rev_char = reverse_brackets[char]
+#                     print(rev_char)
+#                     if s[i-1] in left-set(rev_char):
+#                         return False
+#                     else:
+#                         if valid[rev_char] == rev_char:
+#                             valid[rev_char] = ''
+#                         else:
+#                             return False
+#                 else:   
+#                     print('else')
+#                     return False
+        
+#         check = set(valid.values())
+#         print('valid',valid)
+#         print('check',check)
+#         print('lencheck',len(check))
+        
+#         if check=={''}:
+#             return True
+#         else: 
+#             return False
+        
+
+                   
+        
