c6fb508d5eca2ae5a56b92a5a07d57432428b2dc
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-01-17 00:10:10 +0900

    Time: 35 ms (83.20%), Space: 14.2 MB (36.35%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        
+        def dfsol(open,close,st,ans):
+            print(st)
+            if (open==0 and close ==0):
+                ans.append(st)
+                return
+            
+            if open>0:
+                dfsol(open-1,close,st+"(",ans)
+            
+            if open<close:
+                dfsol(open,close-1,st+")",ans)
+        
+        
+        dfsol(n,n,"",ans)
+        
+        return ans
+        
