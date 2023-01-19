e23ac599335e4c0aef447de8e4ee93395d9110f2
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-01-19 13:15:54 +0900

    Time: 88 ms (80.03%), Space: 15.4 MB (91.96%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,58 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        ans = []
+        check =0
+        mid_point = len(nums)//2
+        init = 0
+        end = len(nums)
+        def bsearch(num_list,targ,ans,mid_point,init,end):
+            if num_list[init:end]:
+                if targ>num_list[mid_point]:
+                    tmp_mid = mid_point+1 + (end-(mid_point+1))//2
+                    bsearch(num_list,targ,ans,tmp_mid,mid_point+1,end)
+                elif targ<num_list[mid_point]:
+                    mid_plus = (mid_point-init)//2
+                    tmp_mid = init+ mid_plus
+                    bsearch(num_list,targ,ans,tmp_mid,init,mid_point)
+                else:
+                    ans.append(mid_point)
+                    tmp_mid1 = init+(mid_point-init)//2
+                    tmp_mid2 = mid_point+1 + (end - (mid_point+1))//2
+                    bsearch(num_list,targ,ans,tmp_mid1,init,mid_point)
+                    bsearch(num_list,targ,ans,tmp_mid2,mid_point+1,end)
+                    return None
+                
+#             mid_index = len(num_list)//2
+#             # abs_index = 
+           
+#             if num_list:
+#                 print(num_list)
+#                 print('ans',ans)
+#                 print('mid',mid_index)
+                
+#                 if targ>num_list[mid_index]:
+#                     bsearch(num_list[mid_index+1:],targ,ans,check)
+#                     check+=1
+#                 elif targ<num_list[mid_index]:
+#                     # print(num_list[:mid_index])
+#                     bsearch(num_list[:mid_index],targ,ans,check)
+#                 else:
+#                     # print(check)
+#                     # if check >0:
+#                     #     ans.append(mid_index+check)
+#                     # else:
+#                     #     ans.append(mid_index)   
+#                     ans.append(mid_index)
+#                     bsearch(num_list[:mid_index],targ,ans,check)
+#                     bsearch(num_list[mid_index+1:],targ,ans,check)
+#             else:
+#                 return None
+        
+        bsearch(nums,target,ans,mid_point,init,end)
+        
+        if ans:
+            ans.sort()
+            return [ans[0],ans[-1]]
+        else:
+            return [-1,-1]
+        
