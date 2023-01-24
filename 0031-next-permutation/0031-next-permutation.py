42baf15e5d4e9d45a061fd529a55b5b596e11d57
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-01-24 22:16:28 +0900

    Time: 41 ms (86.82%), Space: 13.9 MB (19.59%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -6,18 +6,22 @@ class Solution:
         
         """https://leetcode.com/problems/next-permutation/discuss/1909728/Simple-9-line-Python-Solution-with-Detailed-Explanation-(Easy-Understand-for-Beginners!) """
         
+        # 1,4,2,3 
         
+        #corner case
         if nums == sorted(nums,reverse=True):
             nums.sort()
         
         else:
             for i in range(len(nums)-1,0,-1):
                 if nums[i-1]<nums[i]:
+                    print(i) # 3
                     nums[i:] = sorted(nums[i:])
                 
                 swap_pos = -1
                 for j in range(i,len(nums)):
                     if nums[i-1]<nums[j]:
+                        print('swap_pos',j) # 3 과 2 바꾸기 
                         swap_pos = j
                         nums[i-1], nums[swap_pos] = nums[swap_pos],nums[i-1]
                         return nums
