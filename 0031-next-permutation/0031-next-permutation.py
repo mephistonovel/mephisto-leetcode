5b23b85d5ee465a904df04fad037c33e4c9c2003
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-01-24 22:10:17 +0900

    Time: 77 ms (28.30%), Space: 13.9 MB (69.00%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,29 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        """https://leetcode.com/problems/next-permutation/discuss/1909728/Simple-9-line-Python-Solution-with-Detailed-Explanation-(Easy-Understand-for-Beginners!) """
+        
+        
+        if nums == sorted(nums,reverse=True):
+            nums.sort()
+        
+        else:
+            for i in range(len(nums)-1,0,-1):
+                if nums[i-1]<nums[i]:
+                    nums[i:] = sorted(nums[i:])
+                
+                swap_pos = -1
+                for j in range(i,len(nums)):
+                    if nums[i-1]<nums[j]:
+                        swap_pos = j
+                        nums[i-1], nums[swap_pos] = nums[swap_pos],nums[i-1]
+                        return nums
+                        # break 
+                
+                
+                
+                # return nums
+        
