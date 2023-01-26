2e7fb81d8610e6ecb4a1a8d903e473ed8234c1b1
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-01-27 00:00:27 +0900

    Time: 364 ms (39.27%), Space: 28.7 MB (12.08%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -2,14 +2,29 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         if min(nums)>1 or max(nums)<1:
             return 1
-        nums = list(set(nums))
+        nums = list(set(nums)) #set으로 만드는 게 핵심
         nums.sort()
         mid = 0
         
-        for i in range(len(nums)):
-            if nums[i]>0:
-                mid = i
-                break       
+        med = len(nums)//2
+        if min(nums)>0:
+            mid = 0
+        elif nums[med]>0:
+            while med>0 and nums[med]>0:
+                med -= 1
+            mid = med+1
+        else:        
+            while med < len(nums) and nums[med]<=0:
+                med+=1
+            print(med)
+            mid = med
+
+        
+            
+#         for i in range(len(nums)):
+#             if nums[i]>0:
+#                 mid = i
+#                 break       
                 
         return self.check(nums[mid:])
             
