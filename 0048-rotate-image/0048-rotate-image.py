d92ed573e8ce68528ebcb63f8b821a4feb4794fe
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-01-29 10:04:04 +0900

    Time: 42 ms (51.93%), Space: 13.9 MB (67.46%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -7,3 +7,21 @@ class Solution:
         matrix2 = matrix[:]
         for i in range(length):
             matrix[i] = [matrix2[j][i] for j in range(length-1,-1,-1)]
+        
+        ### solution ###
+#         class Solution:
+#             def rotate(self, matrix: List[List[int]]) -> None:
+#                 self.transpose(matrix)
+#                 self.reflect(matrix)
+
+#             def transpose(self, matrix):
+#                 n = len(matrix)
+#                 for i in range(n):
+#                     for j in range(i + 1, n):
+#                         matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i] #python이라 가능한 swapping
+
+#             def reflect(self, matrix):
+#                 n = len(matrix)
+#                 for i in range(n):
+#                     for j in range(n // 2):
+#                         matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j] #
