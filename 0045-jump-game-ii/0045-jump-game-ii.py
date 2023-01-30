b45915e6825817fb8b08d390a6cc8b2fe86abced
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-01-31 00:24:04 +0900

    Time: 129 ms (87.25%), Space: 15 MB (55.64%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,36 +1,46 @@
 from functools import lru_cache
 class Solution:
-    def jump(self, nums: List[int]) -> int:
-        #dp take the jump or not
-        @lru_cache(maxsize = None)
-        def dp(index):
-            if(index >= len(nums)-1):
-                return 0
-            maxval = float("inf")
-            for i in range(1, nums[index]+1):
-                maxval = min(maxval, 1 + dp(index + i))
-            return maxval
-        return dp(0)
+###https://leetcode.com/problems/jump-game-ii/discuss/3106089/five-lines-(9ms)-C%2B%2B-Solutions-(Beats-97.10)###
+    def jump(self,nums: List[int]) -> int:
+        pos = des = 0
+        count = 0
+        for i in range(0,len(nums)-1):
+            des = max(des,nums[i]+i)
+            if (pos==i):
+                pos = des
+                count+=1
+        
+        if pos>=(len(nums)-1):
+            return count
+        return -1
+
+### DP solution ### 
+    # def jump(self, nums: List[int]) -> int:
+    #     #dp take the jump or not
+    #     @lru_cache(maxsize = None)
+    #     def dp(index):
+    #         if(index >= len(nums)-1):
+    #             return 0
+    #         maxval = float("inf")
+    #         for i in range(1, nums[index]+1):
+    #             maxval = min(maxval, 1 + dp(index + i))
+    #         return maxval
+    #     return dp(0)
 
     ### time limit ###
 #     from functools import lru_cache
 #     def jump(self, nums: List[int]) -> int:
 #         length = len(nums)
-#         # goal = length -1 
-        
-#         # minval=len(nums)
 #         last = len(nums)-1
 #         ans = []
         
 #         @lru_cache(maxsize = None) 
 #         def dfs(index,count):
-#             # print(count)
-            
-#             if index == last:
+         
+#             if index >= last:
 #                 ans.append(count)
 #                 return 
             
-#             # minval = float("inf")
 #             jump_max = nums[index] if index < length else 0
 #             for i in range(jump_max,0,-1):
 #                 dfs(index+i,count+1)
