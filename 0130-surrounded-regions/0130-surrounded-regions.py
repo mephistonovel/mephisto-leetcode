a3bc9c0c3c80d6066bf140b3ae1231e86ac21d5b
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-02-16 22:30:11 +0900

    Update 0130-surrounded-regions.py

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,32 +3,69 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        ### https://leetcode.com/problems/surrounded-regions/discuss/3135985/Easy-to-understand-PYTHON-solution-beats-93-with-proper-comments ### 
         rows, cols= len(board), len(board[0])
+        q = deque()
+        
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == 'O':
+                    if i==0 or i == rows-1 or j==0 or j==cols-1:
+                        board[i][j]='C'
+                        q.append((i,j))
+        
+        # finq = deque()
+        # print(q[0])        
+      # 특정 ,i,j에 대해 시행할 bfs즉, 가장자리의 것! 
+        def bfs(q):
+            # visited = set()
+            while q:
+                i,j = q.popleft()
+                for row,col in [[i,j-1],[i-1,j],[i,j+1],[i+1,j]]:
+                    if 0<row<rows and 0<col<cols and board[row][col] == 'O':
+                        board[row][col]='C'
+                        q.append((row,col))
 
-        def capture(r,c): 
-            if (r<0 or r==rows or c<0 or c==cols or board[r][c]!="O"):
-                return 
-            board[r][c]="T"
-            capture(r+1,c)
-            capture(r-1,c)
-            capture(r,c+1)
-            capture(r,c-1)
+        for orow,ocol in q:
+            tmpq = deque()
+            tmpq.append((orow,ocol))
+            bfs(tmpq)
+            
+            
+        # print(board)
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j]=='O':
+                    board[i][j]='X'
+                elif board[i][j]=='C':
+                    board[i][j]='O'
+                    
+        
+        ### https://leetcode.com/problems/surrounded-regions/discuss/3135985/Easy-to-understand-PYTHON-solution-beats-93-with-proper-comments ### 
+#         rows, cols= len(board), len(board[0])
 
-        #(DFS)capture unsurrounded region(0->T)
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c]=="O" and (r==0 or r==rows-1 or c==0 or c==cols-1):
-                    capture(r,c)
+#         def capture(r,c): 
+#             if (r<0 or r==rows or c<0 or c==cols or board[r][c]!="O"):
+#                 return 
+#             board[r][c]="T"
+#             capture(r+1,c)
+#             capture(r-1,c)
+#             capture(r,c+1)
+#             capture(r,c-1)
 
-        #capture surrounded region(0->x)
-        for r in range(1,rows-1):
-            for c in range(1,cols-1):
-                if board[r][c]=="O":
-                    board[r][c]="X" 
+#         #(DFS)capture unsurrounded region(0->T)
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if board[r][c]=="O" and (r==0 or r==rows-1 or c==0 or c==cols-1):
+#                     capture(r,c)
 
-        #uncapture unsurrounded region(T->0)
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c]=="T":
-                    board[r][c]="O" 
+#         #capture surrounded region(0->x)
+#         for r in range(1,rows-1):
+#             for c in range(1,cols-1):
+#                 if board[r][c]=="O":
+#                     board[r][c]="X" 
+
+#         #uncapture unsurrounded region(T->0)
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if board[r][c]=="T":
+#                     board[r][c]="O" 
