fc5af9afdf7e8180173a4d2d8b1c6773c780c46d
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-02-18 23:18:54 +0900

    Time: 49 ms (55.98%), Space: 15.4 MB (92.16%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -8,30 +8,39 @@ class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         if not root:
             return []
+
+        q, paths = deque([(root, targetSum, [])]), []
         
+        while q:
+            cur, target, path = q.pop()  
+            if not (cur.left or cur.right) and cur.val == target:
+                paths.append(path + [cur.val])
+            else:
+                if cur.left:
+                    q.appendleft((cur.left, target - cur.val, path + [cur.val]))
+                if cur.right:
+                    q.appendleft((cur.right, target - cur.val, path + [cur.val]))
+                                 
+        return paths
+### DFS ###
+#         ank = []
         
-        # ans = {targetSum:[]}
-        # vals = []
-        ank = []
-        
-        def dfs(node,tmp,val):
-            if not node.left and not node.right:
-                if val == targetSum:
-                    ank.append(tmp[:])
-                # if sum(tmp[:]) == targetSum:
-                #     ans[targetSum].append(tmp[:])
-                return
+#         def dfs(node,tmp,val):
+#             if not node.left and not node.right:
+#                 if val == targetSum:
+#                     ank.append(tmp[:])
+#                 return
             
-            if node.left:
-                dfs(node.left,tmp+[node.left.val],val+node.left.val)
+#             if node.left:
+#                 dfs(node.left,tmp+[node.left.val],val+node.left.val)
                 
-            if node.right:
-                dfs(node.right,tmp+[node.right.val],val+node.right.val)
+#             if node.right:
+#                 dfs(node.right,tmp+[node.right.val],val+node.right.val)
             
-            tmp = tmp[:-1] # 하...이게 늘 포인트지 dfs의
+#             tmp = tmp[:-1] # 하...이게 늘 포인트지 dfs의
         
-        dfs(root,[root.val],root.val)
+#         dfs(root,[root.val],root.val)
         
-        return ank
+#         return ank
         
         
