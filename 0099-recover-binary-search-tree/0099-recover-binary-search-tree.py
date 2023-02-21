98e180abe95d9a5fb2577c68b6790689e88c6f29
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-02-21 11:09:30 +0900

    Time: 94 ms (16.36%), Space: 14.4 MB (19.33%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
@@ -10,53 +10,27 @@ class Solution:
         Do not return anything, modify root in-place instead.
         """
         # in order활용?
-        change = set()
-        def check(node):
-            ans = []
-            def dfs(node):
-                # print(ans)
-                if not node:
-                    return
-                else:
-                    dfs(node.left)
-                    ans.append(node.val)
-                    dfs(node.right)                    
-                    
-            
-            if node.left:
-                ans = []
+        
+        ans = []
+        
+        def dfs(node):
+            if not node:
+                return
+            else:
                 dfs(node.left)
-                # print(ans)
-                if max(ans)>node.val:
-                    change.add((node.val,max(ans)))
-            if node.right:
-                ans = []
+                ans.append(node.val)
                 dfs(node.right)
-                if min(ans)<node.val:
-                    change.add((node.val,min(ans)))
-                    
         
-        q = deque([root])
+        dfs(root)
+        sort_ans = sorted(ans)
+        ch_pair = []
+        # print(ans)
+        # print(sort_ans)
+        for a1,a2 in zip(ans,sort_ans):
+            if a1!=a2:
+                ch_pair.append(a1)
         
-        while q and not change:
-            lenq = len(q)
-            for i in range(lenq):
-                x = q.popleft()
-                check(x)
-                
-                if x.left:
-                    q.append(x.left)
-                if x.right:
-                    q.append(x.right)
-        
-        # print(change) #{(2,3),(2,1)}
-        change = list(change)
-        q2 = deque([root]) 
-        if len(change) ==1:
-            ch_pair = change[0]
-        if len(change)>1:
-            # if change[0][0] == change[1][0]:
-            ch_pair = (change[0][1],change[1][1])
+        q2 = deque([root])
         while q2: 
             lenq = len(q2)
             for i in range(lenq):
@@ -68,39 +42,69 @@ class Solution:
                     q2.append(x.left)
                 if x.right:
                     q2.append(x.right)
-                    
-#         # def check(numbers,node_val):
-#         root_index = ans.index(root.val)
-#         left = ans[:root_index]
-#         right = ans[root_index+1:]
-
-#         if left and max(left)>root.val:
-#             change_val = max(left)
-#         if right and min(right)<root.val:
-#             change_val = min(right)
-            
-#         # root.val = change_val
-#         # print(change_val)
-#         q = deque([root])
-#         ch = True
         
-#         while q and ch:
-#             qlen = len(q)
-#             for i in range(qlen):
-#                 x = q.popleft() 
-#                 if x.val == change_val:
-#                     # print(1)
-#                     x.val = root.val 
-#                     ch = False 
-#                     break
+
+#############15%defeat##########
+#         change = set()
+#         def check(node):
+#             ans = []
+#             def dfs(node):
+#                 # print(ans)
+#                 if not node:
+#                     return
+#                 else:
+#                     dfs(node.left)
+#                     ans.append(node.val)
+#                     dfs(node.right)                    
+                    
+            
+#             if node.left:
+#                 ans = []
+#                 dfs(node.left)
+#                 # print(ans)
+#                 if max(ans)>node.val:
+#                     change.add((node.val,max(ans)))
+#             if node.right:
+#                 ans = []
+#                 dfs(node.right)
+#                 if min(ans)<node.val:
+#                     change.add((node.val,min(ans)))
+                    
+        
+#         q = deque([root])
+        
+#         while q and not change:
+#             lenq = len(q)
+#             for i in range(lenq):
+#                 x = q.popleft()
+#                 check(x)
                 
 #                 if x.left:
 #                     q.append(x.left)
 #                 if x.right:
 #                     q.append(x.right)
         
-#         root.val = change_val
-            
+#         # print(change) #{(2,3),(2,1)}
+#         change = list(change)
+#         q2 = deque([root]) 
+#         if len(change) ==1:
+#             ch_pair = change[0]
+#         if len(change)>1:
+#             # if change[0][0] == change[1][0]:
+#             ch_pair = (change[0][1],change[1][1])
+#         while q2: 
+#             lenq = len(q2)
+#             for i in range(lenq):
+#                 x = q2.popleft()
+#                 if x.val in ch_pair:
+#                     x.val = list(set(ch_pair)-{x.val})[0]
+
+#                 if x.left:
+#                     q2.append(x.left)
+#                 if x.right:
+#                     q2.append(x.right)
+                    
+
             
         
         
