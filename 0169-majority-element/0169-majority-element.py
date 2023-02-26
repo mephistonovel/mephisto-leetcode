2f8834adf251ccde2af44a749d7dc42f46972285
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-02-27 00:05:15 +0900

    Time: 180 ms (37.52%), Space: 15.6 MB (25.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,12 +2,13 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         if len(nums)==1:
             return nums[0]
+        
         nums.sort()
         std = len(nums)//2 # 양수에서는 floor기능 가능
         
         i=1
         tmp = nums[0]
-        print(nums)
+    
         for l in range(1,len(nums)):
             
             if nums[l]!= tmp:
