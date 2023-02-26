b729004b848e342a314c92a6452a42b59ff0083e
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-02-27 00:53:12 +0900

    Time: 2093 ms (5.02%), Space: 44.1 MB (17.72%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,88 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        ### https://otugi.tistory.com/264 ### 
+        if not head or not head.next:
+            return head
+        
+        p, slow, fast = None, head, head
+        while fast and fast.next:
+            p = slow
+            slow = slow.next
+            fast = fast.next.next
+        p.next = None
+        
+        return self.merge(self.sortList(head), self.sortList(slow))
+    
+    def merge(self,n1,n2):
+        final = ans = ListNode(0,None)
+        
+        while n1 and n2:
+            if n1.val<=n2.val:
+                ans.val = n1.val
+                n1=n1.next
+            else:
+                ans.val = n2.val
+                n2 = n2.next
+            ans.next = ListNode()
+            ans = ans.next
+        
+        while n1 and n1.next:
+            ans.val = n1.val 
+            ans.next=ListNode()
+            ans = ans.next
+            n1 = n1.next
+        
+        while n2 and n2.next:
+            ans.val = n2.val
+            ans.next = ListNode()
+            ans = ans.next
+            n2 = n2.next
+        
+        if n1:
+            ans.val = n1.val
+        if n2:
+            ans.val = n2.val
+        
+        return final
+                
+        
+        
+        
+        
+        #### selection sort는 timelimit #### 
+#         # selection sort
+#         # find_minimum for head[i:]
+#         def find_min(node):
+#             minval = 10**5+1
+#             minnode = None
+            
+#             while node:
+#                 if node.val<minval:
+#                     minval = node.val
+#                     minnode = node
+                
+#                 node = node.next 
+                
+#             return minnode
+        
+#         curr = head
+        
+#         #sort
+#         while curr:
+#             minnode = find_min(curr)
+#             curr.val, minnode.val = minnode.val,curr.val
+#             curr=curr.next
+            
+        
+#         return head
+            
+        
+            
+            
+        
