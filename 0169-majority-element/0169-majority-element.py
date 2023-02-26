b7418793d735057dd492012f6b0424e845e41751
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-02-27 00:10:01 +0900

    Time: 167 ms (77.12%), Space: 15.6 MB (25.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,28 +2,49 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         if len(nums)==1:
             return nums[0]
+        ### no sorting ###
         
-        nums.sort()
-        std = len(nums)//2 # 양수에서는 floor기능 가능
+        check = dict()
+        std = len(nums)//2
         
-        i=1
-        tmp = nums[0]
-    
-        for l in range(1,len(nums)):
-            
-            if nums[l]!= tmp:
-                i=1
+        for n in nums:
+            if n not in check:
+                check[n]=1
             else:
-                i+=1
-            
-            # print(i,nums[l])
-            if i>std:
-                tmp = nums[l]
-                break
-            
-            tmp = nums[l]
+                check[n]+=1
         
-        return tmp
+        print(check)
+        ans = 0
+        for n,item in check.items():
+            if item>std:
+                ans = n
+                break
+        
+        return ans
+        
+        
+        
+        
+        ### sorting #### 
+#         nums.sort()
+#         std = len(nums)//2 # 양수에서는 floor기능 가능
+        
+#         i=1
+#         tmp = nums[0]
+    
+#         for l in range(1,len(nums)):
+            
+#             if nums[l]!= tmp:
+#                 i=1
+#             else:
+#                 i+=1
+#             if i>std:
+#                 tmp = nums[l]
+#                 break
+            
+#             tmp = nums[l]
+        
+#         return tmp
 
             
             
