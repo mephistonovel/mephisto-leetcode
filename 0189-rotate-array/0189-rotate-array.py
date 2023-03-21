d0f5430e7db6dc7f53cbaa136f8999d9199f4167
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-03-21 11:12:35 +0900

    Time: 223 ms (67.13%), Space: 25.5 MB (25.57%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,10 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k=k%len(nums)
-        front = nums[-k:]
-        nums[:] = front+nums[:-k]
-
+        # k=k%len(nums)
+        # front = nums[-k:]
+        # nums[:] = front+nums[:-k]
+        
+        #O(1)
+        #https://leetcode.com/problems/rotate-array/discuss/3244055/Rotation-Array-
+        
+        def reverse(array,low,high):
+            while low<high:
+                tmp = array[low]
+                array[low] = array[high]
+                array[high]=tmp
+                low+=1
+                high-=1
+        k = k%len(nums)
+        n = len(nums)-1
+        reverse(nums,0,n)
+        reverse(nums,0,k-1)
+        reverse(nums,k,n)
         # if len(nums)<=1 or k==len(nums):
         #     pass
         # elif len(nums)-k<=1:
