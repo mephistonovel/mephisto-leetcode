a373b2aa42a731b316e85a0b80781764c95077b0
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-03-21 11:12:53 +0900

    Time: 228 ms (56.11%), Space: 25.4 MB (74.09%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -22,21 +22,3 @@ class Solution:
         reverse(nums,0,n)
         reverse(nums,0,k-1)
         reverse(nums,k,n)
-        # if len(nums)<=1 or k==len(nums):
-        #     pass
-        # elif len(nums)-k<=1:
-        #     tmp0 = nums[0]
-        #     nums[0]=nums[1]
-        #     for i in range(1,len(nums)-1):
-        #         nums[i] = nums[i+1]
-        #     nums[-1] = tmp0
-        # else:
-        #     for i in range(k,0,-1):
-        #         print(k,i)
-        #         tmp = nums[-i] # nums[-3]
-        #         robbed = nums[k-i] # nums[3-3]=nums[0]
-        #         swap = nums[k-i+k] # nums[3+3-3] = nums[3]
-        #         nums[k-i] = tmp
-        #         nums[-i]=swap
-        #         nums[k+k-i]=robbed
-            
