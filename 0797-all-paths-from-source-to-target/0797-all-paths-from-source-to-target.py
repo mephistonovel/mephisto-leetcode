6281590bf48ed08f46313b5fdde6a9315199e9f5
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-03-23 20:12:46 +0900

    Time: 101 ms (73.77%), Space: 15.7 MB (73.79%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,35 +1,35 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        paths = []
-        visited = set()
-        end = len(graph)-1
-        def dfs(node, path):
-            visited.add(node)
-            path.append(node)
+#         paths = []
+#         visited = set()
+#         end = len(graph)-1
+#         def dfs(node, path):
+#             visited.add(node)
+#             path.append(node)
 
-            if node == end:
-                paths.append(path[:])
-            else:
-                for neighbor in graph[node]:
-                    if neighbor not in visited:
-                        dfs(neighbor, path)
+#             if node == end:
+#                 paths.append(path[:])
+#             else:
+#                 for neighbor in graph[node]:
+#                     if neighbor not in visited:
+#                         dfs(neighbor, path)
 
-            visited.remove(node)
-            path.pop()
+#             visited.remove(node)
+#             path.pop()
 
-        dfs(0, [])
-        return paths
-#         ans = []
-#         des = len(graph)-1
-#         def dfs(node,path):
-#             if node == des:
-#                 ans.append(path[:])
+#         dfs(0, [])
+#         return paths
+        ans = []
+        des = len(graph)-1
+        def dfs(node,path):
+            if node == des:
+                ans.append(path[:])
 
-#             for neighbor in graph[node]:
-#                 path.append(neighbor)
-#                 dfs(neighbor,path)
-#                 path.pop()
+            for neighbor in graph[node]:
+                path.append(neighbor)
+                dfs(neighbor,path)
+                path.pop()
 
          
-#         dfs(0,[0])
-#         return ans
+        dfs(0,[0])
+        return ans
