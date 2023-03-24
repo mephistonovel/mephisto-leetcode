6bb5b51481dd34d2bbdca8fe9adbd759bb2e6785
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2023-03-24 20:26:16 +0900

    Time: 1247 ms (26.12%), Space: 14 MB (22.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,14 +2,10 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) >len(s2):
             return False
-        # if len(s1)==len(s2):
-        #     return set(s1)==set(s2)
-        
-        check = False
+
         step = len(s1)
         s1count = Counter(s1)
-        # print(s1count)
-        # for i in range(0,len(s2)-step+1):
+
         i=0
         while i<len(s2)-step+1:    
             s22=Counter(s2[i:i+step])
@@ -17,6 +13,5 @@ class Solution:
                 return True
             i+=1 
         return False
-        
-        # return check
+
         
